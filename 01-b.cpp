f8b80f879726dbf5af0ef72f50a300275f700a84
Percy Qaz <percyqaz@yahoo.co.uk>
2024-12-02 00:53:49 +0000

    Day 1b Improved

--- 01-b.cpp
+++ 01-b.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
-
-using int_list = std::vector<int>;
+#include <unordered_map>
 
 __int64 solution_01_b(const char* input)
 {
 	std::ifstream file(input);
 	_ASSERT(file.is_open());
 	std::string line;
-	int_list left_numbers = std::vector<int>();
-	int_list right_numbers = std::vector<int>();
+	auto lefts = std::unordered_map<int, int>();
+	auto rights = std::unordered_map<int, int>();
 	while (std::getline(file, line))
 	{
 		std::istringstream iss(line);
 		int left, right;
 		if (!(iss >> left >> right)) { break; }
 
-		left_numbers.push_back(left);
-		right_numbers.push_back(right);
+		lefts[left]++;
+		rights[right]++;
 	}
 	int similarity{ 0 };
-	int n = left_numbers.size();
-	for (int i = 0; i < n; i++)
+	for (auto iter = lefts.begin(); iter != lefts.end(); iter++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			if (left_numbers[i] == right_numbers[j])
-			{
-				similarity += left_numbers[i];
-			}
-		}
+		std::pair kvp{ *iter };
+		similarity += kvp.first * kvp.second * rights[kvp.first];
 	}
 	return similarity;
 }
