faa6182ea5ea24bd7cd94c5203e13adac490dce7
Percy Qaz <percyqaz@yahoo.co.uk>
2024-12-03 13:20:43 +0000

    Extend scaffolding for future days

--- main.cpp
+++ main.cpp
@@ -8,14 +8,17 @@ int main()
 	time_it(solution_01_a, "input/01-input.txt", 2196996);
 	time_it(solution_01_b, "input/01-example.txt", 31);
 	time_it(solution_01_b, "input/01-input.txt", 23655822);
+	std::cout << std::endl;
 
 	time_it(solution_02_a, "input/02-example.txt", 2);
 	time_it(solution_02_a, "input/02-input.txt", 334);
 	time_it(solution_02_b, "input/02-example.txt", 4);
 	time_it(solution_02_b, "input/02-input.txt", 400);
+	std::cout << std::endl;
 
 	time_it(solution_03_a, "input/03-example.txt", 161);
 	time_it(solution_03_a, "input/03-input.txt", 189600467);
 	time_it(solution_03_b, "input/03-example.txt", 48);
 	time_it(solution_03_b, "input/03-input.txt", 107069718);
+	std::cout << std::endl;
 }
--- solutions.h
+++ solutions.h
@@ -8,3 +8,68 @@ __int64 solution_02_b(const char* input);
 
 __int64 solution_03_a(const char* input);
 __int64 solution_03_b(const char* input);
+
+__int64 solution_04_a(const char* input);
+__int64 solution_04_b(const char* input);
+
+__int64 solution_05_a(const char* input);
+__int64 solution_05_b(const char* input);
+
+__int64 solution_06_a(const char* input);
+__int64 solution_06_b(const char* input);
+
+__int64 solution_07_a(const char* input);
+__int64 solution_07_b(const char* input);
+
+__int64 solution_08_a(const char* input);
+__int64 solution_08_b(const char* input);
+
+__int64 solution_09_a(const char* input);
+__int64 solution_09_b(const char* input);
+
+__int64 solution_10_a(const char* input);
+__int64 solution_10_b(const char* input);
+
+__int64 solution_11_a(const char* input);
+__int64 solution_11_b(const char* input);
+
+__int64 solution_12_a(const char* input);
+__int64 solution_12_b(const char* input);
+
+__int64 solution_13_a(const char* input);
+__int64 solution_13_b(const char* input);
+
+__int64 solution_14_a(const char* input);
+__int64 solution_14_b(const char* input);
+
+__int64 solution_15_a(const char* input);
+__int64 solution_15_b(const char* input);
+
+__int64 solution_16_a(const char* input);
+__int64 solution_16_b(const char* input);
+
+__int64 solution_17_a(const char* input);
+__int64 solution_17_b(const char* input);
+
+__int64 solution_18_a(const char* input);
+__int64 solution_18_b(const char* input);
+
+__int64 solution_19_a(const char* input);
+__int64 solution_19_b(const char* input);
+
+__int64 solution_20_a(const char* input);
+__int64 solution_20_b(const char* input);
+
+__int64 solution_21_a(const char* input);
+__int64 solution_21_b(const char* input);
+
+__int64 solution_22_a(const char* input);
+__int64 solution_22_b(const char* input);
+
+__int64 solution_23_a(const char* input);
+__int64 solution_23_b(const char* input);
+
+__int64 solution_24_a(const char* input);
+__int64 solution_24_b(const char* input);
+
+__int64 solution_25(const char* input);
